cacfecf9ddacf7fe95299030ee393e386b01bc53
FELIPE AMARAL DE SOUZA <felipe.asouza29@senacsp.edu.br>
2025-08-19 16:47:44 -0300

    alteracoes na main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,7 +28,11 @@ export default function Navbar() {
                 </ul>
                 </li>
                 <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                <a class="nav-link"href"#/register">Cadastre-se</a>
+                </li>
+
+                <li class="nav-item">
+                <a class="nav-link" href= "#/login">Login</a>
                 </li>
             </ul>
             <form class="d-flex" role="search">
--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import renderRegisterPage from "./pages/register.js";
 
 document.addEventListener('DOMContentLoaded', () => { 
    renderRegisterPage();
+   //Novas Páginas aqui adicionadas conforme desenvolvidas
 });
+
+function getPath(){
+   //obtem o hash(ex"#/login"), remove o # e tira os espaços
+   const url = (location.hash || "").replace(/^ #,"").trim();
+
+   return url && url.startsWith("/")? url : "/login";
+}
+
+function renderRouter(){
+   const url = getPath();
+   const render = routes[url] || routes["/login"];
+}
